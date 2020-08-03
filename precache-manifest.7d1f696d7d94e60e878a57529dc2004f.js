f8bf2fb0fa8be3601ce29d77cdcb4a3acf5e9e06
Chrystian Guth <chrystian.guth@csghq.com>
2020-08-03 18:36:15 -0300

    Updates

---
+++ precache-manifest.7d1f696d7d94e60e878a57529dc2004f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cd0e53b81600d724b0143562f8af6f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8f776c5cf98a9e3afdf",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5fb06f84550e3a97a5a5",
+    "url": "/static/js/2.c38a6de6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c38a6de6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8f776c5cf98a9e3afdf",
+    "url": "/static/js/main.42a5714b.chunk.js"
+  },
+  {
+    "revision": "92af1fd704b1ff78aaf7",
+    "url": "/static/js/runtime-main.a5365a4d.js"
+  }
+]);
